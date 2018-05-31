33d655caeef00be6c6dd58d1b443a3f5726fe476
honghongzhu <honghong033@hotmail.com>
2018-05-31 11:05:47 +0200

    initial commit

---
+++ src/markovdecisionproblem/DrawFrame.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package markovdecisionproblem;
+
+import java.awt.Container;
+import javax.swing.JFrame;
+
+/**
+ * Draws the given MDP.
+ * @author Sjoerd Lagarde + some adaptations by Jered Vroon
+ *
+ */
+public class DrawFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private int width = 750;
+	private int height = 800;
+	private MarkovDecisionProblem mdp;
+
+	/**
+	 * Constructor.
+	 * @param mdp
+	 */
+	public DrawFrame(MarkovDecisionProblem mdp) {
+		this.mdp = mdp;
+		width = mdp.getWidth() * 50;
+		height = mdp.getHeight() * 50;
+		setSize(width+20, height+100);
+		setTitle("MDP Visualization");
+		
+		drawContent();
+	}
+	
+	/**
+	 * Adds the content to the frame:
+	 */
+	public void drawContent() {
+		DrawPanel panel = new DrawPanel(mdp, width, height);
+		Container contentPane = getContentPane();
+		contentPane.add(panel);		
+	}
+	
+}
